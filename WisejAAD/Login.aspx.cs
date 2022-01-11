33cae9201c5a958b9077ba130f2a1309cac975da
Tung Ngo <ngo-thanh.tung@spc-technology.com>
2022-01-11 02:13:23 -0500

    update

--- WisejAAD/Login.aspx.cs
+++ WisejAAD/Login.aspx.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.Owin.Security;
-using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OpenIdConnect;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace WisejAAD
 {
